a26bfaf39836828aec8c2c5f82e9f63eb8d42eff
Mario Blanco <mario10_bd@hotmail.com>
2023-10-21 21:21:33 +0200

    database creator missing

--- app/src/main/java/es/upm/miw/bantumi/integration/GameRoomDatabase.java
+++ app/src/main/java/es/upm/miw/bantumi/integration/GameRoomDatabase.java
@@ -1,19 +1,29 @@
 package es.upm.miw.bantumi.integration;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+
+import es.upm.miw.bantumi.BantumiApp;
 
 @Database(entities = {Game.class}, version = 2, exportSchema = false)
 @TypeConverters({Converters.class})
 public abstract class GameRoomDatabase extends RoomDatabase {
     private static GameRoomDatabase INSTANCE;
 
-    public static GameRoomDatabase getDatabase() {
+    public static GameRoomDatabase getDatabase(Context context) {
+        if(INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(
+                    context,
+                    GameRoomDatabase.class,
+                    "games")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
         return INSTANCE;
     }
-
     public abstract GameDAO gameDao();
-
-
 }
